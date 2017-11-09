b2ace8800dcea59778c13b7bb2a14b4502b5a13e
AlmeidaCaio <almeida.caio1@gmail.com>
2017-11-09 11:30:15 -0200

    terceiro

---
+++ mostrarPessoal.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Ficha de Pessoal</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        .format {
+            padding-top:20px;
+        }
+    </style>
+    <script>
+        function acceptDateEmpty() {
+            var date = document.getElementById("date");
+            if (date.value == '') {
+                date.value = "NULL";
+            }
+        }
+    </script>
+</head>
+
+<body>
+
+    <script src="js/common.js"></script>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            <center>Ficha de Pessoal</center>
+                        </h1>
+                    </div>
+                </div><br>
+                <!-- /.row -->
+                
+                <?php 
+                    include_once 'database.php';
+                    $cod = $_POST['cod'];
+                    $matricula = $_POST['matricula'];
+                    $da = $_POST['da'];
+                    $ng = $_POST['ng'];
+                    $trigrama = $_POST['trigrama'];
+                    $aqs = $_POST['aqs'];
+                    $pg = $_POST['pg'];
+                    $forca = $_POST['forca'];
+                    $po = $_POST['po'];
+                    $cf = $_POST['cf'];
+                    $omo = $_POST['omo'];
+                    $rm = $_POST['rm'];
+                    
+                    $nome = $_POST['nome'];
+                    $email = $_POST['email'];
+                    $dn = $_POST['dn'];
+                    $ec = $_POST['ec'];
+                    $naturalidade = $_POST['naturalidade'];
+                    $nacionalidade = $_POST['nacionalidade'];
+                    $cpf = $_POST['cpf'];
+                    $rg = $_POST['rg'];
+                    $oe = $_POST['oe'];
+                    $de = $_POST['de'];
+                    $im = $_POST['im'];
+                    $preccp = $_POST['preccp'];
+                    $te = $_POST['te'];
+                    $zona = $_POST['zona'];
+                    $secao = $_POST['secao'];
+                    $pai = $_POST['pai'];
+                    $mae = $_POST['mae'];
+
+                    $rua = $_POST['rua'];
+                    $numero = $_POST['numero'];
+                    $complemento = $_POST['complemento'];
+                    $bairro = $_POST['bairro'];
+                    $cidade = $_POST['cidade'];
+                    $uf = $_POST['uf'];
+
+                    if (isset($_POST['cadastroPessoal'])) {
+                    
+                    // Para Back-End
+                    /*
+                        $dao = new Database();
+                        $dao->uploadPhoto($_FILES["fileToUpload"]["name"], $_FILES["fileToUpload"]["tmp_name"]);
+                        $sql = "insert into titular (dataInscricao, validade, tipoPermissao, posto, nome, identidade, orgaoExpedidor, 
+                            dataExpedicao, tipo, estadoCivil, cpf, nascimento, telResidencial, celular, email, empresa, cargo, telComercial) 
+                            values ('$dataInscricao', '$validade', '$tipoPermissao', '$posto', '$nome', '$idt', '$orgaoExpedidor', 
+                                    '$dataExpedicao', '$tipo', '$estadoCivil', '$cpf', '$nasc', '$telResidencial', '$celular', '$email', 
+                                    '$empresa', '$cargo', '$telComercial');";
+                        $sql = str_replace("''", "NULL", $sql);
+                        if ($dao->query($sql)) 
+                            $id = mysqli_insert_id($dao->link);
+//                            echo "<script>getElementById('nrInscricao').innerHTML = " . $id + 1 . ";</script>";
+                            $sqlEndereco = "insert into endereco (cod, numero, complemento, bairro, cidade, uf) values ('$id', '$numero', 
+                                '$complemento', '$bairro', '$cidade', '$uf');";
+                            $dao->query($sqlEndereco);
+                            */
+                    }
+                ?>    
+
+                <script src="js/formBlock.js"></script>
+                            
+            </div>
+
+        </div>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+
+</body>
+
+</html>
